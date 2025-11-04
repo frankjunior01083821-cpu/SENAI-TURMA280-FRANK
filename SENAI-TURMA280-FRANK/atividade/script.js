9737e8b767740f0a69ef1ba2092e2345f375f3d2
frankjunior01083821-cpu <frank.junior01083821@sesisenaipr.org.br>
2025-11-04 16:55:00 -0300

    Add files via upload

---
+++ SENAI-TURMA280-FRANK/atividade/script.js
@@ -0,0 +1,5 @@
+function cadastrar(){
+    const usuario = document .getElementById('usuario').value
+    const senha = document .getElementById('senha').value
+    const confirmarsenha = document .getElementById('confirmarSenha')
+}
